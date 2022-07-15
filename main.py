631ad4602e7db457dc90ebb0b6c23100ad6db762
Luis Rigoberto Gallardo Estrella <lrgallardo+programming@gmail.com>
2022-07-15 20:35:17 +0000

    added the daily wage function

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 # get user name
 u_name = input('What\'s your name?: ')
+
+# get user hourly wage
+h_wage = int(input('What\'s your hourly wage?: $'))
+
+#daily wage function
+def daily_wage():
+  d_wage = 8 * h_wage
+  print(f'Your daily salary is ${d_wage}')
+
+daily_wage()
